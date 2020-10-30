2d90e891704126c2e930629b8a78b8558759f636
Osamuyimen Ogiku <ogiku85@yahoo.co.uk>
2020-10-30 23:22:34 +0100

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react';
+// import * as s from 'semantic-ui-react/src';
+// import {Icon} from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state={
+    values:[]
+  };
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/values').then((response) =>{
+      this.setState({
+        values: response.data
+      });
+    });
+    this.setState(
+      {
+        values: [{id:1, name: 'Value 101'},{id:2, name: 'Value 102'}]
+      });
+  }
+  render(){
+    return (
+      <div>
+        <h1>Working</h1>
+          <Header as='h2'>
+            <Icon name='users' />
+            <Header.Content>Reactivities</Header.Content>
+          </Header>
+          <List>
+            {this.state.values.map((value : {id:number, name: string}) => {
+                return <List.Item key={value.id}>{value.name}</List.Item>
+              })}
+          </List>
+      </div>
+    );
+  }
 }
 
 export default App;
+
